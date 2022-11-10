04b6259084a742eb3101430b59e04ca06afba20f
unknown <ttn.hdttb152004@gmail.com>
2022-11-10 14:39:36 +0700

    update

--- ss8_ss9_if_else_switch_case/luyen_tap_cau_truc_dieu_kien_2_bai_11/luyen_tap_cau_truc_dieu_kien_2_bai_11.js
+++ ss8_ss9_if_else_switch_case/luyen_tap_cau_truc_dieu_kien_2_bai_11/luyen_tap_cau_truc_dieu_kien_2_bai_11.js
@@ -3,9 +3,9 @@ function bn(){
     if (thu_nhap <= 5000000){
         document.getElementById("result").innerHTML = "Thu nhập cá nhân của bạn là: " + thu_nhap*90/100 + "VNĐ"
     }else if (thu_nhap <= 10000000){
-        document.getElementById("result").innerHTML = "Thu nhập cá nhân của bạn là: " + thu_nhap*80/100 + "VNĐ"
+        document.getElementById("result").innerHTML = "Thu nhập cá nhân của bạn là: " + (thu_nhap*80/100 - 250000) + "VNĐ"
     }else if (thu_nhap <= 15000000){
-        document.getElementById("result").innerHTML = "Thu nhập cá nhân của bạn là: " + thu_nhap*70/100 + "VNĐ"
+        document.getElementById("result").innerHTML = "Thu nhập cá nhân của bạn là: " + (thu_nhap*70/100 - 750000) + "VNĐ"
     }else {
         document.getElementById("result").innerHTML = "Làm gì nhiều tiền để đâu cho hết."
     }
