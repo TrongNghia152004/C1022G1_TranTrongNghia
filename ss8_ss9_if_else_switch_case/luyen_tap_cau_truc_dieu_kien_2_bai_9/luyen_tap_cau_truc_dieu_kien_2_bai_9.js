2b554abd58d544f7f10f0034bd0ab6df0f9b2159
unknown <ttn.hdttb152004@gmail.com>
2022-11-10 15:23:47 +0700

    update

--- ss8_ss9_if_else_switch_case/luyen_tap_cau_truc_dieu_kien_2_bai_9/luyen_tap_cau_truc_dieu_kien_2_bai_9.js
+++ ss8_ss9_if_else_switch_case/luyen_tap_cau_truc_dieu_kien_2_bai_9/luyen_tap_cau_truc_dieu_kien_2_bai_9.js
@@ -1,8 +1,8 @@
 function hehe(){
-    let a = document.getElementById("soa").value;
-    let b = document.getElementById("sob").value;
-    let c = document.getElementById("soc").value;
-    if (a,b,c > 0){
+    let a = +document.getElementById("soa").value;
+    let b = +document.getElementById("sob").value;
+    let c = +document.getElementById("soc").value;
+    if (a >0 && b > 0 && c > 0){
             }if (a + b > c && a + c > b && b + c > a){
         document.getElementById("result").innerHTML = "a,b,c là cạnh tam giác"
              }else {
